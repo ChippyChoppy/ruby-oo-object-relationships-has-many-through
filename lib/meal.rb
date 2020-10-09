cd0b6ad8b74c2c32a5b8e966d3040dfc0f99698f
ChippyChoppy <rebeccamargaret13@gmail.com>
2020-10-08 22:42:10 -0400

    soigne? soigne.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,24 @@
+#this will be our joining model between our waiter & customer classes.
+#waiters have many customers through meals
+#customers have many waiters through meals
+#Meal class will be our single source of truth
+
 class Meal
 
+    attr_accessor :waiter, :customer, :total, :tip
+
+    @@all = []
+
+    def initialize(waiter, customer, total, tip=0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self 
+    end
+
+    def self.all
+        @@all
+    end
+    
 end
